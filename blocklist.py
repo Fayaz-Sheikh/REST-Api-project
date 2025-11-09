105d14496780cfffecda999a209dde88b28fc565
Fayaz Sheikh <fayazsheikh10.3.1998@gmail.com>
2025-11-09 12:25:23 +0530

    First Commi!

---
+++ blocklist.py
@@ -0,0 +1,6 @@
+"""
+blocklist.py
+This file will contain the blocklist  of the JWT tokens.It will be imported  by app  and the logout  resource so that tokens can be added to the blocklist when the user logouts.
+"""
+# blocklist.py
+BLOCKLIST = set()
